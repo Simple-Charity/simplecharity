6ee6ff9b857caeae386b75acb64beebb90cedd68
Andrew Forrester <atf@fastmail.com>
2022-10-22 10:19:37 -0400

    podcast entry improvements

--- src/vue/PodcastPlayer.vue
+++ src/vue/PodcastPlayer.vue
@@ -1,27 +1,38 @@
 <template>
-    <div>
-        <div class="w-full ">
-            <div class="main-carousel md:px-20 max-w-[60rem]">
-                <div class="carousel-cell px-2" v-for="transcriptSuperBlock in transcript">
-                    <div class="pb-8 " v-for="transcriptBlock in transcriptSuperBlock">
-                        <div class="flex items-center justify-between pb-4">
-                            <div class="flex items-center">
-                                <div class="pr-4">
-                                    <img class="h-12 w-12 rounded-full" :src="transcriptBlock.speakerHeadshot" alt="HS">
-                                </div>
-                                <div class="text-blue text-xl">
-                                    {{ transcriptBlock.speakerName }}
-                                </div>
+    <div class="hidden md:block">
+        <!--Need to build a modal transcript viewer for small screens-->
+        <div class="pb-6">
+            <h1 class="text-blue text-2xl font-bold">Read the transcript</h1>
+        </div>
+        <div class="h-[45rem] overflow-hidden relative rounded-lg bg-linen">
+            <div class="absolute left-0 right-0 top-0 h-[5rem] bg-linen z-20 flex justify-center items-center">
+                <div class="h-[2.5rem] w-full relative">
+                    <input v-model="searchTerm" class="absolute inset-0 border-b-2 ml-12 mr-[3.15rem] border-blue text-lg" type="text" placeholder="Search">
+                    <div v-on:click="resetSearch()" class="absolute right-[3.15rem] top-0 bottom-0 w-[2rem] flex items-center justify-end hover:cursor-pointer">
+                        <h2 class="text-blue text-xl font-bold">&#10005;</h2>
+                    </div>
+                </div>
+            </div>
+            <div id="transcriptviewer" class="w-full h-[40rem] overflow-y-scroll bg-linen pl-12 pr-10 pt-4 relative mt-20 rounded-b-lg">
+            
+                <div class="pb-8" v-for="transcriptBlock in selectedTranscriptBlocks">
+                    <div class="flex items-center justify-between pb-2">
+                        <div class="flex items-center">
+                            <div class="pr-4">
+                                <img class="h-12 w-12 rounded-full" :src="transcriptBlock.speakerHeadshot" alt="HS">
                             </div>
-                        
-                            <div class="text-blue text-xl opacity-[0.38]">
-                                {{ transcriptBlock.timestamp }}
+                            <div class="text-blue text-xl font-bold">
+                                {{ transcriptBlock.speakerName }}
                             </div>
                         </div>
                     
-                        <div class="text-blue text-lg">
-                            {{ transcriptBlock.text }}
+                        <div class="text-blue text-xl opacity-[0.38]">
+                            {{ transcriptBlock.timestamp }}
                         </div>
+                    </div>
+                
+                    <div class="text-blue text-lg">
+                        {{ transcriptBlock.text }}
                     </div>
                 </div>
             </div>
@@ -37,25 +48,21 @@
         props: ["transcript"],
         data: function() {
             return {
-
+                searchTerm: "",
+                transcriptBlocks: this.transcript,
             } 
         },
 
         methods: {
-
+            resetSearch() {
+                this.searchTerm = "";
+            }
         },
-
-        mounted () {
-            var elem = document.querySelector('.main-carousel');
-            var flkty = new Flickity( elem, {
-            cellAlign: 'left',
-            pageDots: false,
-            wrapAround: false,
-            friction: 0.3,
-            prevNextButtons: true,
-            adaptiveHeight: true,
-        });
-    }
+        computed: {
+            selectedTranscriptBlocks() {
+                return this.transcriptBlocks.filter(transcriptBlock => transcriptBlock.text.toLowerCase().indexOf(this.searchTerm.toLowerCase()) != -1 || transcriptBlock.speakerName.toLowerCase().indexOf(this.searchTerm.toLowerCase()) != -1);
+            }
+        }
     }
 
 </script>
